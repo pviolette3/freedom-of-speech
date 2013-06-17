92e2fc698ba817e3801877a110d686483910379c
Patrick <pviolette3@gatech.edu>
2013-06-17 13:37:25 +0000

    Moved host name to chat.ejs. app defaults to local mode. now to get the javascripts and css in a separate file.

--- app.js
+++ app.js
@@ -1,30 +1,28 @@
 var express = require('express');
 var socketio = require('socket.io');
 
-var app = express();
-var os = require('os');
-console.log('hostname ' + os.hostname());
+var app = express(),
+  http = require('http'),
+  server = http.createServer(app);
 
-function startServer() { 
-  var PORT = 8080;
-  process.argv.forEach(function(val, index, array) {
-    if(val == 'prod') {
-      PORT = 80;
-    }
-  });
-  app.set('view engine', 'ejs')
-  app.configure(function() {
-    app.use(express.methodOverride());
-    app.use(express.bodyParser());
-  //  app.use(express.static(__dirname + '/public'));
-    app.use(app.router);
-  });
+var PORT = 8080;
+process.argv.forEach(function(val, index, array) {
+  if(val == 'prod') {
+    PORT = 80;
+  }
+});
+app.set('view engine', 'ejs')
+app.configure(function() {
+  app.use(express.methodOverride());
+  app.use(express.bodyParser());
+//  app.use(express.static(__dirname + '/public'));
+  app.use(app.router);
+});
 
-  var server = app.listen(PORT);
-  console.log("" + server.address().port);
-};
-
-startServer();
+server.listen(PORT);
+console.log("" + server.address().port);
+io = socketio.listen(server);
+io.set('log level', 10);
 
 app.get('/', function(req, res) {
   console.log('host', req.host);
@@ -32,14 +30,14 @@ app.get('/', function(req, res) {
 });
 
 var users = {}
-var io = socketio.listen(app);
 io.sockets.on('connection', function(socket) {
-  
   socket.on('sendchat', function(data) {
+    console.log('got send chat');
     io.sockets.emit('updatechat', socket.user, data);
   });
 
   socket.on('adduser', function(username) {
+    console.log("got add user");
     socket.user = username;
     users[username] = username; //add, but don't duplicate
     socket.emit('updatechat', 'SERVER', 'you have connected');
@@ -48,6 +46,7 @@ io.sockets.on('connection', function(socket) {
   });
 
   socket.on('disconnect', function(){
+    console.log('got disconnect');
     delete users[socket.user];
     io.sockets.emit('updateusers', users);
     socket.broadcast.emit('updatechat', 'SERVER', socket.user + ' has left.');
