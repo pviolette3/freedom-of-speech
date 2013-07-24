d41ac1d4a38c4a0868397f2dca100574f3c350cb
Patrick Violette <patrick@localhost.localdomain>
2013-07-23 21:37:38 -0400

    Made shell script collect files into archives for re calculating greylist

--- ml/iterate.sh
+++ ml/iterate.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
+export directory=archive/$(date "+%F%T")
+mkdir $directory 
+mkdir $directory/uncensored
+mkdir $directory/censored
+cat *uncensored.txt > un2ensored.txt
+mv *uncensored.txt $directory/uncensored/
+
+cat *censored.txt > 2ensored.txt
+mv *censored.txt $directory/censored/
+
+mv 2ensored.txt censored.txt
+mv un2ensored.txt uncensored.txt
 python grey_list_generator.py > greylist.txt
-rm uncensored.txt
-rm censored.txt
 python weights_generator.py > weights.txt
+mv censored.txt $directory/censored/
+mv uncensored.txt $directory/uncensored/
